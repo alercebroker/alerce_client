816b3ec87788f19e192a390dc317b5146fa7c430
Camilo Valenzuela <cacke91@gmail.com>
2019-09-03 17:33:28 -0400

    Changed name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-   name='ALeRCE API',
+   name='alerce',
    version='0.1',
    description='Package to access to ALeRCE data',
    author='ALeRCE Team',
    author_email='contact@alerce.online',
-   packages=['alerce_api'],  #same as name
+   packages=['alerce'],  #same as name
    install_requires=['pandas', 'requests', 'IPython'], #external packages as dependencies
 )
 
