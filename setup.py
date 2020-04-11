4481d64e2de8e462cf12c1a899cd41888b6a9327
Ignacio Reyes <ireyesj93@gmail.com>
2020-04-11 09:07:16 -0400

    feat: requests done using a Session, which improves performace when many queries are done. MIT License. setup.py has more info. fix: Some PEP8 fixes, not fully fixed.

--- setup.py
+++ setup.py
@@ -3,13 +3,17 @@ with open("requirements.txt") as f:
     required_packages = f.readlines()
 
 setup(
-   name='alerce',
-   version='0.2.3',
-   description='Package to access to ALeRCE data',
-   author='ALeRCE Team',
-   author_email='contact@alerce.online',
-   packages=['alerce'],  #same as name
-   install_requires=required_packages, 
-
+    name='alerce',
+    version='0.2.4',
+    description='Package to access to ALeRCE data',
+    author='ALeRCE Team',
+    author_email='contact@alerce.online',
+    license='MIT',
+    classifiers=[
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'License :: OSI Approved :: MIT License'
+    ],
+    url="https://github.com/alercebroker/alerce_client",
+    packages=['alerce'],  # same as name
+    install_requires=required_packages,
 )
-
