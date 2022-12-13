c42a8c30a9d1023cfdabb3312654d41e4e470af7
ASHuenchuleo <arsepulh@gmail.com>
2022-12-13 10:54:49 -0300

    chore: Set up semantic release for version 1.1.0
    Updated requirements-dev.txt accordingly and added a .cfg file for
    python-semantic-release.

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     required_packages = f.readlines()
 
+__version__ = "1.1.0"
+
 setup(
    name='alerce',
-   version="1.1.0",
+   version=__version__",
    description='ALeRCE Client',
    author='ALeRCE Team',
    author_email='contact@alerce.online',
