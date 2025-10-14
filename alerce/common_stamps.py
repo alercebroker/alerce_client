f79ef261efd1d199c23ab02fc368077712e090f7
Claudio Mansilla <c.mansillabrito@gmail.com>
2025-10-14 09:47:35 -0300

    changed error to warning when the survey is not specified. eliminated kwargs

--- alerce/common_stamps.py
+++ alerce/common_stamps.py
@@ -1,7 +1,7 @@
 from .ms_stamps import AlerceStampsMultisurvey
 from .stamps import AlerceStamps
 from .utils import Client
-
+import warnings
 
 class AlerceCommonStamps(Client):
     def __init__(self, **kwargs):
@@ -11,51 +11,53 @@ class AlerceCommonStamps(Client):
         self.multisurvey_stamps_client = AlerceStampsMultisurvey(**ms_stamp_config)
 
     def plot_stamps(
-        self, oid, candid=None, use_multisurvey_api=False, survey=None, **kwargs
+        self, oid, candid=None, survey=None
     ):
-        if use_multisurvey_api:
-            if survey is None:
-                raise ValueError(
-                    "survey must be provided when use_multisurvey_api is True"
-                )
-            if not hasattr(self.multisurvey_stamps_client, "multisurvey_plot_stamps"):
-                raise NotImplementedError("Multisurvey plot_stamps not implemented.")
+        
+        if survey is None:
+            survey = "ztf"
+            warnings.warn(
+                "survey not provided, defaulting to 'ztf'. This will use the legacy ZTF client. This behavior will be deprecated in future versions.",
+                DeprecationWarning,
+            )
+
+        if survey == "ztf":
+            return self.legacy_stamps_client.plot_stamps(
+                oid=oid, candid=candid, format=format
+            )
+        elif survey in self.valid_surveys:
             return self.multisurvey_stamps_client.multisurvey_plot_stamps(
-                survey=survey, oid=oid, candid=candid, **kwargs
+                oid=oid, candid=candid, survey=survey, format=format
             )
         else:
-            return self.legacy_stamps_client.plot_stamps(oid, candid=candid, **kwargs)
+            raise ValueError(f"survey must be one of {self.valid_surveys}")
 
     def get_stamps(
         self,
         oid,
         candid=None,
         format="HDUList",
-        use_multisurvey_api=False,
         survey=None,
-        **kwargs
     ):
-        if use_multisurvey_api:
-            if survey is None:
-                raise ValueError(
-                    "survey must be provided when use_multisurvey_api is True"
-                )
-            if not hasattr(self.multisurvey_stamps_client, "multisurvey_get_stamps"):
-                raise NotImplementedError("Multisurvey get_stamps not implemented.")
+        if survey is None:
+            survey = "ztf"
+            warnings.warn(
+                "survey not provided, defaulting to 'ztf'. This will use the legacy ZTF client. This behavior will be deprecated in future versions.",
+                DeprecationWarning,
+            )
+
+        if survey == "ztf":
+            return self.legacy_stamps_client.get_stamps(
+                oid=oid, candid=candid, format=format
+            )
+        elif survey in self.valid_surveys:
             return self.multisurvey_stamps_client.multisurvey_get_stamps(
-                survey=survey, oid=oid, candid=candid, format=format, **kwargs
+                oid=oid, candid=candid, survey=survey, format=format
             )
         else:
-            # Only pass supported arguments to legacy method
-            legacy_kwargs = {}
-            if candid is not None:
-                legacy_kwargs["candid"] = candid
-            if format is not None:
-                legacy_kwargs["format"] = format
-            return self.legacy_stamps_client.get_stamps(oid, **legacy_kwargs)
-
+            raise ValueError(f"survey must be one of {self.valid_surveys}")
     def get_avro(
-        self, oid, candid=None, use_multisurvey_api=False, survey=None, **kwargs
+        self, oid, candid=None, use_multisurvey_api=False, survey=None
     ):
         if use_multisurvey_api:
             if survey is None:
@@ -65,7 +67,7 @@ class AlerceCommonStamps(Client):
             if not hasattr(self.multisurvey_stamps_client, "multisurvey_get_avro"):
                 raise NotImplementedError("Multisurvey get_avro not implemented.")
             return self.multisurvey_stamps_client.multisurvey_get_avro(
-                survey=survey, oid=oid, candid=candid, **kwargs
+                survey=survey, oid=oid, candid=candid
             )
         else:
-            return self.legacy_stamps_client.get_avro(oid, candid=candid, **kwargs)
+            return self.legacy_stamps_client.get_avro(oid, candid=candid)
