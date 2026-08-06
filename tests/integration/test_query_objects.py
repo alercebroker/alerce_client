9974903f6a4a620159286893fe4e0674f68c11c0
demurest <eric.apolonio@hotmail.com>
2026-08-06 10:27:38 -0400

    fix(integration_test): fix integration test

--- tests/integration/test_query_objects.py
+++ tests/integration/test_query_objects.py
@@ -41,7 +41,7 @@ def test_query_objects_lsst_multisurvey_1(client):
 
 
 def test_query_objects_lsst_multisurvey_classifier_without_class_name(client):
-    params = {"classifier": "stamp_classifier_rubin", "n_det": [5, 10]}
+    params = {"classifier": "stamp_classifier_rubin_beta", "n_det": [5, 10]}
     result = client.query_objects(survey="lsst", format="json", **params)
     result_items_df = pd.DataFrame(result)
     print("Multisurvey LSST objects:")
